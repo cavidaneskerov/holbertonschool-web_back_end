db12145abd36cadfc95acd5e120d695833dcb565
cavidaneskerov <cavidansgrv06@gmail.com>
2025-11-30 18:24:48 +0400

    Update 9-insert_school.py

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -2,5 +2,6 @@
 """Python with mongodb"""
 
 def insert_school(mongo_collection, **kwargs):
+    """Insert new elements"""
     new_doc = mongo_collection.insert_one(kwargs)
     return new_doc.inserted_id
