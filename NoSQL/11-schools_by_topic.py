e96c0625a439ace4b5b8efd447b5d219a659da82
cavidaneskerov <cavidansgrv06@gmail.com>
2025-11-30 18:33:13 +0400

    Update 11-schools_by_topic.py

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""Select with specifics"""
+"""Select with specifics df sdfdsf sd"""
 
 def schools_by_topic(mongo_collection, topic):
-    """Select with pymongo"""
+    """Select with pymongo fsdf dsf dfs"""
    return list(mongo_collection.find({ "topic": topic}))
