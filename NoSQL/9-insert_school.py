14782284cd44113b7d91e451c0f6f61f0992bbe1
cavidaneskerov <cavidansgrv06@gmail.com>
2025-11-30 18:24:30 +0400

    Create 9-insert_school.py

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Python with mongodb"""
+
+def insert_school(mongo_collection, **kwargs):
+    new_doc = mongo_collection.insert_one(kwargs)
+    return new_doc.inserted_id
